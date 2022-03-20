f79f4dbd7b70f332dc88bf5bab0418d2f39fcd62
Diego Córdova <d.cordovaratti@gmail.com>
2022-03-20 19:40:29 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+
+
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtdGIVjN2POosbE5mZGAv0XHZbDf-cKtc",
+  authDomain: "authentification-eaa14.firebaseapp.com",
+  projectId: "authentification-eaa14",
+  storageBucket: "authentification-eaa14.appspot.com",
+  messagingSenderId: "1048209798305",
+  appId: "1:1048209798305:web:0334314b390c86bb7dece0"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+export default auth
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import auth from '../main'
 
 Vue.use(VueRouter)
 
@@ -8,22 +9,41 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {authRequired: true}
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/login',
+    name: 'LogIn',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (LogIn.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import(/* webpackChunkName: "LogIn" */ '../views/LogIn.vue')
+  },
+  {
+    path: '/signin',
+    name: 'SignIn',
+    // route level code-splitting
+    // this generates a separate chunk (SignIn.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "SignIn" */ '../views/SignIn.vue')
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const { currentUser } = auth
+  const { meta: { authRequired } } = to
+
+  if (currentUser && authRequired) next()
+  else if (!currentUser && authRequired) next("/login")
+  else if (currentUser && !authRequired) next("/signin")
+  else next()
 })
 
 export default router
